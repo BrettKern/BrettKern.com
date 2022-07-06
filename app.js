e65405d64cec9ce7a73a06d122cf3c0ac27e35c4
Admin <admin@Admins-MBP.home>
2022-07-05 23:00:31 -0400

    updated middlewhere delcarations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const ejs = require('ejs')
 const partials = require('express-partials')
 const express = require('express')
+const bodyParser = require('body-parser')
 var app = express();
 
 var routes = require('./routes/index.js');
@@ -8,8 +9,8 @@ var routes = require('./routes/index.js');
 app.use(partials());
 app.engine('html', require('ejs').renderFile); //renders .ejs as html
 
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
+app.use(express.static('./public'));
 
 app.use(express.bodyParser()); //deals with incoming request objects
 app.use(express.methodOverride());
