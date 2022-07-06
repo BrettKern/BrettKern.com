c4edc9d17733db6f95c513d01271ea2dc3eb0ef8
Admin <admin@Admins-MBP.home>
2022-07-05 23:09:51 -0400

    .

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ var app = express();
 
 var routes = require('./routes/index.js');
 
-app.use(partials());
+//app.use(partials());
 app.engine('html', require('ejs').renderFile); //renders .ejs as html
 
 app.set('views', './views');
 app.use(express.static('./public'));
-
-app.use(express.bodyParser()); //deals with incoming request objects
-app.use(express.methodOverride());
-
-/**** Turn on some debugging tools ****/
-app.use(express.logger()); // sends messages into the terminal
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); //dumpExceptions - directs exceptions to stderr - showStack - generate HTML for an exception å
-
+//
+// app.use(express.bodyParser()); //deals with incoming request objects
+// app.use(express.methodOverride());
+//
+// /**** Turn on some debugging tools ****/
+// app.use(express.logger()); // sends messages into the terminal
+// app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); //dumpExceptions - directs exceptions to stderr - showStack - generate HTML for an exception å
+//
 
 
 app.get('/', routes.main);
